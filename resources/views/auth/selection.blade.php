aa7b39df33a166d97d6aff93ce4ff1aff9047acb
mohamedelbadawi <38269461+mohamedelbadawi@users.noreply.github.com>
2022-08-18 01:41:43 +0100

    Update selection.blade.php

--- resources/views/auth/selection.blade.php
+++ resources/views/auth/selection.blade.php
@@ -17,7 +17,7 @@
         href="https://fonts.googleapis.com/css?family=Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900">
 
     <!-- css -->
-    <link href="{{ URL::asset('assets/css/ltr.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/rtl.css') }}" rel="stylesheet">
 
 
 
